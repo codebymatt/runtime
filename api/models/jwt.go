d87210ab3ee1e74386630fe2ec1a3f0c6cbe479f
codebymatt <mattcraig365@gmail.com>
2018-11-10 17:10:17 +0000

    Start writing authorization tests

--- api/models/jwt.go
+++ api/models/jwt.go
@@ -1,1 +1,10 @@
 package models
+
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+type Claim struct {
+	email string
+	jwt.StandardClaims
+}
--- api/models/jwt_test.go
+++ api/models/jwt_test.go
@@ -1,1 +1,46 @@
 package models
+
+import (
+	"testing"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func TestValidClaimIsValidated(t *testing.T) {
+	claim := Claim{
+		"mgscott@dundermifflin.com",
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Second * 10).Unix(),
+		},
+	}
+
+	err := claim.Valid()
+
+	if err != nil {
+		t.Error("Claim should be valid")
+	}
+}
+
+func TestInvalidClaimIsInvalid(t *testing.T) {
+	claim := Claim{
+		"mgscott@dundermifflin.com",
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Second * -10).Unix(),
+		},
+	}
+
+	err := claim.Valid()
+
+	if err == nil {
+		t.Error("Claim should be invalid")
+	}
+}
+
+func TestJWTIsDecodedProperly(t *testing.T) {
+
+}
+
+func TestJWTIsEncodedProperly(t *testing.T) {
+
+}

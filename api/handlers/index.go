390a5250ea36637d18b0d702425833b712fe4adb
codebymatt <mattcraig365@gmail.com>
2018-11-13 22:04:06 +0000

    Set Content-Type header properly

--- api/handlers/index.go
+++ api/handlers/index.go
@@ -6,8 +6,8 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
 	message := "Everything's fine!"
 	responseBody := createJSONResponse(http.StatusOK, message)

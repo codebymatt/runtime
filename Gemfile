774c096781a85f6e6966e73d2b7625a89b6120b1
codebymatt <mattcraig365@gmail.com>
2018-11-13 08:01:54 +0000

    Continue working on migration script

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source "https://rubygems.org"
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # Added at 2018-11-12 21:47:13 +0000 by matt:
 gem "rubocop", "~> 0.60.0"
--- api/scripts/db/migrate.rb
+++ api/scripts/db/migrate.rb
@@ -2,15 +2,32 @@
 
 require 'pg'
 
-runtime_env = ENV['RUNTIME_ENV']
-db_name     = ENV["RUNTIME_#{runtime_env}_DB"]
-db_password = ENV["RUNTIME_DB_#{runtime_env}_PASSWORD"]
-db_user     = ENV["RUNTIME_#{runtime_env}_USER"]
+RUNTIME_ENV = ENV['RUNTIME_ENV']
+DBNAME      = ENV["RUNTIME_#{RUNTIME_ENV}_DB"]
+PASSWORD    = ENV["RUNTIME_DB_#{RUNTIME_ENV}_PASSWORD"]
+USER        = ENV["RUNTIME_#{RUNTIME_ENV}_USER"]
 
-connection_String = "dbname=#{db_name} user=#{db_user}"
+CHECK_IF_TABLE_EXISTS = "SELECT EXISTS (
+    SELECT 1
+    FROM   information_schema.tables
+    WHERE  table_name = '#{DBNAME}'
+);"
+
+MIGRATIONS_FOLDER = File.join(__dir__, 'migrations')
+
+connection_string = "dbname=#{DBNAME} user=#{USER} password=#{PASSWORD}"
+migrations = Dir.entries(MIGRATIONS_FOLDER) - ['.', '..']
+
+def run_migrations(start_index) end
+
 begin
     connection = PG.connect(connection_string)
-    puts connection.server_version
+    if connection.exec(CHECK_IF_TABLE_EXISTS).fields[0] == 'exists'
+        # Get most recent migration num
+        # run_migrations(migration_rum)
+    else
+        run_migrations(0)
+    end
 rescue PG::Error => e
     puts e
 ensure

d7b9637fc9eb44c2d50afdeeaae136276d16d2ac
codebymatt <mattcraig365@gmail.com>
2018-11-22 08:10:13 +0000

    Add test for user data deserialization

--- api/handlers/users.go
+++ api/handlers/users.go
@@ -2,8 +2,13 @@ package handlers
 
 import (
 	"net/http"
+	"runtime/api/models"
 )
 
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func deserializeUser([]byte) (models.User, error) {
+	return models.User{}, nil
 }
--- api/handlers/users_test.go
+++ api/handlers/users_test.go
@@ -8,15 +8,24 @@ import (
 	"testing"
 )
 
+var testUserData = `{
+	"first_name":"Dwight,
+	"last_name":"Schrute"
+	"email":"manager@schrutefarms.org",
+	"password":"youllneverguessthismose"
+}`
+
+var testUserDataWithID = `{
+	"id": 100000,
+	"first_name":"Dwight,
+	"last_name":"Schrute"
+	"email":"manager@schrutefarms.org",
+	"password":"youllneverguessthismose"
+}`
+
+// ITODO: is this an integration/acceptance test?
 func TestShouldCreateUser(t *testing.T) {
-	data := `{
-			"user": {
-				"first_name":"Dwight,
-				"last_name":"Schrute"
-				"email":"manager@schrutefarms.org",
-				"password":"youllneverguessthismose"
-			}
-		}`
+	data := testUserData
 
 	formattedData := strings.NewReader(data)
 
@@ -38,4 +47,30 @@ func TestShouldCreateUser(t *testing.T) {
 
 	utils.CheckStatusAndContentTypeOk(t, rec)
 	utils.AssertStringsMatch(t, expectedBody, rec.Body.String())
+}
+
+func TestUserJSONDataShouldBeDeserializedSuccessfully(t *testing.T) {
+	data := []byte(testUserData)
+	user, err := deserializeUser(data)
+
+	if err != nil {
+		t.Errorf("Unmarshalling of user failed: %v", err)
+	}
+
+	if user.Email != "manager@schrutefarms.org" {
+		t.Errorf("Unmarshalling of user failed, email field is incorrect")
+	}
+}
+
+func TestUserIdShouldNeverBeDeserialized(t *testing.T) {
+	data := []byte(testUserDataWithID)
+	user, err := deserializeUser(data)
+
+	if err != nil {
+		t.Errorf("Unmarshalling of user failed: %v", err)
+	}
+
+	if user.Id == 100000 {
+		t.Errorf("Unmarshalling of user failed, id field should never be unmarshaled")
+	}
 }

1b399ddf3fe62bbf035222f3b2f3d356ad6160cb
codebymatt <mattcraig365@gmail.com>
2018-11-27 07:19:07 +0000

    Add tests to Retrieve User handler

--- api/handlers/users_retrieve_test.go
+++ api/handlers/users_retrieve_test.go
@@ -31,3 +31,47 @@ func TestShouldRetrieveUser(t *testing.T) {
 	utils.CheckStatusAndContentTypeOk(t, rec)
 	utils.AssertStringsMatch(t, expectedBody, rec.Body.String())
 }
+
+func TestShouldNotRetrieveUserWithExpiredJWT(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/users", nil)
+	bearerToken := "Bearer " + constants.InvalidExpiredToken
+
+	req.Header.Set("Content-Type", constants.ApiContentType)
+	req.Header.Set("Authentication", bearerToken)
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(ts.CheckContentType(ts.AuthorizeRequest(ts.RetrieveUserHandler)))
+
+	handler.ServeHTTP(rec, req)
+	expectedBody := `{"Status":401,"Message":"invalid_session"}`
+
+	authHeader := rec.Header().Get("Authentication")
+	if strings.TrimPrefix(authHeader, "Bearer ") != "" {
+		t.Error("Did not expect JWT to be present")
+	}
+
+	utils.CheckContentTypeOk(t, rec)
+	utils.AssertStringsMatch(t, expectedBody, rec.Body.String())
+}
+
+func TestShouldNotRetrieveUserWithInvalidJWT(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/users", nil)
+	bearerToken := "Bearer " + constants.InvalidSignedToken
+
+	req.Header.Set("Content-Type", constants.ApiContentType)
+	req.Header.Set("Authentication", bearerToken)
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(ts.CheckContentType(ts.AuthorizeRequest(ts.RetrieveUserHandler)))
+
+	handler.ServeHTTP(rec, req)
+	expectedBody := `{"Status":401,"Message":"invalid_session"}`
+
+	authHeader := rec.Header().Get("Authentication")
+	if strings.TrimPrefix(authHeader, "Bearer ") != "" {
+		t.Error("Did not expect JWT to be present")
+	}
+
+	utils.CheckContentTypeOk(t, rec)
+	utils.AssertStringsMatch(t, expectedBody, rec.Body.String())
+}

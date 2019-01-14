5fcb5b47ddad09b4ecbab775a4a86aab82f6afb3
codebymatt <mattcraig365@gmail.com>
2019-01-14 20:58:07 +0000

    Add login API call and redirect to dash

--- web/src/main/components/LoginCard.js
+++ web/src/main/components/LoginCard.js
@@ -10,18 +10,25 @@ class LoginCard extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { "email": "", "password": "" };
+    this.state = { 'email': '', 'password': '' };
   }
 
   authenticateUser = () => {
-    const data = { "credentials": { "email": this.state.email, "password": this.state.password } }
-    axios.post("http://localhost:3000/v1/login.json", data).then((response) => {
-      console.log(response);
+    const data = { 'credentials': { 'email': this.state.email, 'password': this.state.password } }
+    axios.post('http://localhost:3000/v1/login.json', data).then((response) => {
       this.props.history.push('/dashboard');
     }).catch((error) => {
       console.log(error);
     })
   };
+
+  handleEmailChange = (event) => {
+    this.setState({ 'email': event.target.value })
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({ 'password': event.target.value })
+  }
 
   render() {
     return(
@@ -31,18 +38,22 @@ class LoginCard extends Component {
             className='long-input'
             type='email'
             name='email'
+            value={ this.state.email }
+            onChange={ this.handleEmailChange }
             placeholder='Email'
           />
           <input
             className='long-input'
             type='password'
             name='password'
+            value={ this.state.password }
+            onChange={ this.handlePasswordChange }
             placeholder='Password'
           />
           <SubmitButton
             onPress={this.authenticateUser}
             className='submit-button'
-            text="Login"
+            text='Login'
           />
         </form>
       </div>

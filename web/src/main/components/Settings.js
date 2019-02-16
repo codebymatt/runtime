d21f565f2b101927831d1ec40a0fe55ed4e6304a
codebymatt <mattcraig365@gmail.com>
2019-02-16 19:50:21 +0000

    Add confirmation on frontend user delete action

--- web/src/main/components/Settings.js
+++ web/src/main/components/Settings.js
@@ -10,11 +10,55 @@ class Settings extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      confirmingDeletion: false,
       userReference: this.props.user,
       firstName: this.props.user.firstName,
       lastName: this.props.user.lastName,
       email: this.props.user.email
      }
+  }
+
+  render() {
+    return(
+      <div className='settings-wrapper'>
+        <Header />
+        <div className='settings-body'>
+          <h1>Your Settings</h1>
+          <div className='update-user-card'>
+            <form className='update-user-form'>
+              <div className='input-wrapper'>
+                <label>First Name</label>
+                <input
+                  type='text'
+                  name='firstName'
+                  value={ this.state.firstName || '' }
+                  onChange={ this.updateUserData }
+                />
+              </div>
+              <div className='input-wrapper'>
+              <label>Last Name</label>
+                <input
+                  type='text'
+                  name='lastName'
+                  value={ this.state.lastName || '' }
+                  onChange={ this.updateUserData }
+                />
+              </div>
+              <div className='input-wrapper'>
+                <label>Email</label>
+                <input
+                  type='text'
+                  name='email'
+                  value={ this.state.email || '' }
+                  onChange={ this.updateUserData }
+                />
+              </div>
+              {this.chooseButtonDisplay()}
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   updateUserData = (event) => {
@@ -69,56 +113,50 @@ class Settings extends Component {
     });
   }
 
-  render() {
-    return(
-      <div className='settings-wrapper'>
-        <Header />
-        <div className='settings-body'>
-          <h1>Your Settings</h1>
-          <div className='update-user-card'>
-            <form className='update-user-form'>
-              <div className='input-wrapper'>
-                <label>First Name</label>
-                <input
-                  type='text'
-                  name='firstName'
-                  value={ this.state.firstName || '' }
-                  onChange={ this.updateUserData }
-                />
-              </div>
-              <div className='input-wrapper'>
-              <label>Last Name</label>
-                <input
-                  type='text'
-                  name='lastName'
-                  value={ this.state.lastName || '' }
-                  onChange={ this.updateUserData }
-                />
-              </div>
-              <div className='input-wrapper'>
-                <label>Email</label>
-                <input
-                  type='text'
-                  name='email'
-                  value={ this.state.email || '' }
-                  onChange={ this.updateUserData }
-                />
-              </div>
-              <div className='button-wrapper'>
-                <SubmitButton
-                  onPress={this.updateUser}
-                  className='submit-button'
-                  text='Update'
-                />
-                <div className="basic-button delete-button" onClick={ this.deleteAccount }>
-                  Delete
-                </div>
-              </div>
-            </form>
+  toggleDeletionConfirmation = () => {
+    if (this.state.confirmingDeletion) {
+      this.setState({confirmingDeletion: false})
+    } else {
+      this.setState({confirmingDeletion: true})
+    }
+  }
+
+  chooseButtonDisplay = () => {
+    if (this.state.confirmingDeletion) {
+      return(
+        <div className='button-wrapper'>
+          <div
+            onClick={this.deleteAccount}
+            className='basic-button confirm-deletion'
+            text='Update'
+          >
+            Delete
+          </div>
+          <div
+            className="basic-button delete-button"
+            onClick={ this.toggleDeletionConfirmation }
+          >
+            Cancel
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className='button-wrapper'>
+          <SubmitButton
+            onPress={this.updateUser}
+            className='submit-button'
+            text='Update'
+          />
+          <div
+            className="basic-button delete-button"
+            onClick={ this.toggleDeletionConfirmation }
+          >
+            Delete
+          </div>
+        </div>
+      );
+    }
   }
 }
 

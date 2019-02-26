36905ea5c53a4573a5012d1f32e1902a3a6e22c8
codebymatt <mattcraig365@gmail.com>
2019-02-26 22:30:42 +0000

    Redesign Landing page, make mobile responsive

--- web/src/main/components/Landing.js
+++ web/src/main/components/Landing.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import LandingInfo from './LandingInfo';
-import LoginPrompt from './LoginPrompt';
-import SignupCard from './SignupCard';
 
 import './Landing.sass';
+import LandingGraphic from '../../images/runtime-landing.png';
 
 class Landing extends React.Component {
     render() {
       return (
         <div className='landing-wrapper'>
-          <LandingInfo />
-          <SignupCard setUser={ this.props.setUser } />
-          <LoginPrompt />
+          <div className='landing-header'>
+            <div className='logo-wrapper'>
+              runtime
+            </div>
+            <div className='nav-wrapper'>
+              <div class='nav-item about-wrapper'>
+                <p>About</p>
+              </div>
+              <div class='nav-item'>
+                <p>Login</p>
+              </div>
+              <div class='nav-item rt-button nav-button sign-up-button'>
+                Sign Up
+              </div>
+            </div>
+          </div>
+          <div className='landing-graphic'>
+            <img src={LandingGraphic} alt='' />
+          </div>
+          <div className='landing-kicker'>
+            Run, track your times, <br />
+            then visualise them.
+          </div>
+          <div className='sign-up-wrapper'>
+            <p className='cta'>Create an account for free!</p>
+            <div className='rt-button sign-up-button'>
+              Sign Up
+            </div>
+          </div>
+          <div className='landing-footer'>
+          <div className='inner-wrapper'>
+            <div className='my-kicker'>
+                created with care by <a href='https://mattcraig.me'>Matt Craig</a>
+              </div>
+              <div className='social-wrapper'>
+                <a href='https://github.com/codebymatt/runtime'><i className="fab fa-github" /></a>
+                <a href='mailto:contact@mattcraig.me'><i className="fas fa-envelope" /></a>
+              </div>
+            </div>
+          </div>
         </div>
       );
     }
